d5ab67c52b79c97acbe8fbb5aa6201cafc32bf33
Kirti Aggarwal <kirti057btcse21@igdtuw.ac.in>
2022-11-18 06:56:17 -0800

    initial

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import Phaser from "phaser";
+const config={
+  type: Phaser.AUTO,
+  width:800,
+  height:600,
+  physics:{
+    default:'arcade'
+  },
+  scene:{
+    preload:preload,
+    create:create,
+    update:update
+  
+  }
+}
+function preload()
+{
+this.load.image('sky','assets/sky.png');
+this.load.image('bird','assets/bird.png');
+
+}
+let bird=null;
+function create()
+{this.add.image(0,0,'sky').setOrigin(0,0);
+bird=this.physics.add.sprite(config.width*0.1,config.height/2,'bird').setOrigin(0);
+bird.body.velocity.y=200;
+
+}
+function update(){
+
+}
+new Phaser.Game(config);
